9ea27447aeea23171d118f850b92ee316686c3bd
YI ZHANG <scaccterran@gmail.com>
2018-10-31 02:05:33 -0400

    database will now load credentials from global configuration object

--- database.js
+++ database.js
@@ -1,6 +1,8 @@
+const config = global.config.dbConfig;
+const connectionString = `mongodb://${config.host}/${config.name}`;
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/snooker_data', { useNewUrlParser: true });
+mongoose.connect(connectionString, { useNewUrlParser: true });
 
 module.exports = mongoose.connection;
