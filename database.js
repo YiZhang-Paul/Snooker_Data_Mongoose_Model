6b4599a263b3f81f5556897bab61bd7bf3f0f92c
YI ZHANG <scaccterran@gmail.com>
2018-11-07 10:44:55 -0500

    include user name and password in connection string

--- database.js
+++ database.js
@@ -1,11 +1,23 @@
+function getConnectionString(dbConfig) {
+
+    if (process.env.NODE_ENV === 'production') {
+
+        const user = process.env.DB_USER;
+        const password = process.env.DB_PASSWORD;
+
+        return `mongodb://${user}:${password}@${config.host}/${config.name}`;
+    }
+
+    return `mongodb://${config.host}/${config.name}`;
+}
+
 if (global.config && global.config.dbConfig) {
 
     const config = global.config.dbConfig;
-    const connectionString = `mongodb://${config.host}/${config.name}`;
     const mongoose = require('mongoose');
 
     mongoose.Promise = global.Promise;
-    mongoose.connect(connectionString, { useNewUrlParser: true });
+    mongoose.connect(getConnectionString(config), { useNewUrlParser: true });
 
     module.exports = mongoose.connection;
 }
